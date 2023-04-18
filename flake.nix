73142d152caf2aacc8203074afcbc67fa98f2cdd
jfvillablanca <31008330+jfvillablanca@users.noreply.github.com>
2023-04-18 17:35:26 +0800

    Install shell dependencies derived from flake

---
+++ flake.nix
@@ -0,0 +1,32 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    flake-compat = {
+      url = "github:edolstra/flake-compat";
+      flake = false;
+    };
+    devshell.url = "github:numtide/devshell";
+  };
+
+  outputs = { self, nixpkgs, flake-utils, devshell, ... }:
+    flake-utils.lib.eachDefaultSystem (system: {
+      devShell =
+        let
+          pkgs = import nixpkgs {
+            inherit system;
+            overlays = [ devshell.overlays.default ];
+          };
+        in
+        pkgs.devshell.mkShell {
+          name = "Sivis Stockkeeping and Shipment";
+          commands = [
+            {
+              name = "node";
+              package = pkgs.nodejs;
+            }
+          ];
+          env = [ ];
+        };
+    });
+}
