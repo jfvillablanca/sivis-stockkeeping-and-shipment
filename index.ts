45a6a8f0c63ccdc6ee73f9b38fb47d9fbff48f0b
jfvillablanca <31008330+jfvillablanca@users.noreply.github.com>
2023-04-19 14:41:28 +0800

    Enable middlewares

--- index.ts
+++ index.ts
@@ -1,13 +1,26 @@
 import express, { Express, Request, Response } from "express";
+import path from "path";
 import dotenv from "dotenv";
+import createError from "http-errors";
+import cookieParser from "cookie-parser";
+import logger from "morgan";
 
 dotenv.config();
 
 const app: Express = express();
 const port = process.env.PORT;
 
-app.get("/", (req: Request, res: Response) => {
-    res.send("Express + TypeScript Server");
+app.set("views", path.join(__dirname, "views"))
+app.set("view engine", "hbs");
+
+app.use(logger("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser())
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use(function(req, res, next) {
+    next(createError(404));
 });
 
 app.listen(port, () => {
