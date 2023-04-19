df81024a9d26a66b431ea054a7386a489e6a8091
jfvillablanca <31008330+jfvillablanca@users.noreply.github.com>
2023-04-19 15:26:51 +0800

    Add routes and views

---
+++ routes/index.ts
@@ -0,0 +1,6 @@
+import express from "express";
+export const router = express.Router();
+
+router.get("/", function (req, res, next) {
+    res.render("index", { title: "Typescript + Express" });
+});
