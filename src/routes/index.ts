0648d6a506a17eceb18af2e3fecf309586b60f4f
jfvillablanca <31008330+jfvillablanca@users.noreply.github.com>
2023-04-20 17:33:15 +0800

    Render home template

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,5 +2,8 @@ import express from "express";
 export const router = express.Router();
 
 router.get("/", function (req, res, next) {
-    res.render("index", { title: "Typescript + Express" });
+    res.render("index", {
+        title: "Typescript + Express",
+        text: "Welcome text",
+    });
 });
