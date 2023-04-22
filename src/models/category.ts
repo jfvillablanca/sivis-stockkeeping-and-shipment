e64c3837eb634eb27f291d5df0e9bfb738b3a6e8
jfvillablanca <31008330+jfvillablanca@users.noreply.github.com>
2023-04-22 19:43:43 +0800

    Remove enum restriction for Category

--- src/models/category.ts
+++ src/models/category.ts
@@ -6,7 +6,6 @@ const Schema = mongoose.Schema;
 const CategorySchema = new Schema({
     name: {
         type: String,
-        enum: ["Weapon", "Armor", "Potion", "Scroll", "Wondrous Item"],
         required: true,
     },
 });
