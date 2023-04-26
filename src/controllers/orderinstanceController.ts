a8988ee37f60df8feb224763c3e06fd2877dfe96
jfvillablanca <31008330+jfvillablanca@users.noreply.github.com>
2023-04-26 08:44:02 +0800

    Implement new order instance page

--- src/controllers/orderinstanceController.ts
+++ src/controllers/orderinstanceController.ts
@@ -1,5 +1,8 @@
+import { RequestHandler } from "express";
 import OrderInstance from "../models/orderinstance";
 import asyncHandler from "express-async-handler";
+import { body, validationResult } from "express-validator";
+import MagicItem from "../models/magicitem";
 
 // Display list of all Order Instances.
 export const orderinstance_list = asyncHandler(async (req, res, next) => {
@@ -33,13 +36,58 @@ export const orderinstance_detail = asyncHandler(async (req, res, next) => {
 
 // Display Order Instance create form on GET.
 export const orderinstance_create_get = asyncHandler(async (req, res, next) => {
-    res.send("NOT IMPLEMENTED: Order Instance create GET");
+    const magicItemEnumValues = (
+        await MagicItem.find().sort({ item_name: 1 }).exec()
+    ).map((magicitem) => magicitem.item_name);
+
+    res.render("orderinstance_form", {
+        layout: "main",
+        title: "Create new order instance",
+        header: "Create new order instance",
+        magicItemEnumValues,
+    });
 });
 
 // Handle Order Instance create on POST.
-export const orderinstance_create_post = asyncHandler(async (req, res, next) => {
-    res.send("NOT IMPLEMENTED: Order Instance create POST");
-});
+export const orderinstance_create_post: RequestHandler[] = [
+    body("magic_item.*").escape(),
+    body("order_quantity", "Order quantity must be a minimum value of 1")
+        .trim()
+        .isInt({ min: 1, allow_leading_zeroes: false })
+        .escape(),
+
+    asyncHandler(async (req, res, next) => {
+        const errors = validationResult(req);
+        const orderinstance = new OrderInstance({
+            orderArray: [
+                {
+                    magic_item: await MagicItem.findOne({
+                        item_name: req.body.magic_item,
+                    }).exec(),
+                    order_quantity: req.body.order_quantity,
+                },
+            ],
+        });
+
+        if (!errors.isEmpty()) {
+            const magicItemEnumValues = (
+                await MagicItem.find().sort({ item_name: 1 }).exec()
+            ).map((magicitem) => magicitem.item_name);
+
+            res.render("orderinstance_form", {
+                layout: "main",
+                title: "Create new order instance",
+                header: "Create new order instance",
+                magicItemEnumValues,
+                errors: errors.array(),
+            });
+            return;
+        } else {
+            await orderinstance.save();
+            res.redirect(orderinstance.url);
+        }
+    }),
+];
 
 // Display Order Instance delete form on GET.
 export const orderinstance_delete_get = asyncHandler(async (req, res, next) => {
